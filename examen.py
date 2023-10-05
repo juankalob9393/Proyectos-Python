103f0a54560fc082fe7d58206f8fd865ec1876e1
juankalob9393 <juan08carlos9393@gmail.com>
2023-10-04 22:12:29 -0600

    proyecto

--- examen.py
+++ examen.py
@@ -51,49 +51,49 @@
 #         f"No es necesario que declares impuestos {nombre} ya que tus ingresos son menores a $3000.00 y no eres mayor de edad.")
 
 # ---------------------------------------------------------------------------------TEST DE ACOMODO DE GRUPO POR SEXO MODIFICADO------------------------------------------------------------
-# while True:
+while True:
 
-#     Mujeres_grpA = []
-#     Hombres_grpB = []
+    Mujeres_grpA = []
+    Hombres_grpB = []
 
-#     while True:
-#         sexo = input(
-#             "Ingresa el sexo de la persona, (M/H) o (Salir) para no continuar: ")
-#         sexo = str(sexo.capitalize())
+    while True:
+        sexo = input(
+            "Ingresa el sexo de la persona, (M/H) o (Salir) para no continuar: ")
+        sexo = str(sexo.capitalize())
 
-#         if sexo.capitalize() == "Salir":
-#             break
-#         elif sexo.capitalize() != "H" and sexo.capitalize() != "M":
-#             print("No es correcto ingresa un sexo ya sea Hombre (H) o mujer (M)")
-#             continue
+        if sexo.capitalize() == "Salir":
+            break
+        elif sexo.capitalize() != "H" and sexo.capitalize() != "M":
+            print("No es correcto ingresa un sexo ya sea Hombre (H) o mujer (M)")
+            continue
 
-#         nombre = input(
-#             "Ingresa un nombre del alumno o (Salir) para no continuar: ")
-#         nombre = str(nombre.capitalize())
+        nombre = input(
+            "Ingresa un nombre del alumno o (Salir) para no continuar: ")
+        nombre = str(nombre.capitalize())
 
-#         if nombre == "Salir":
-#             break
+        if nombre == "Salir":
+            break
 
-#         if sexo == "M":
-#             Mujeres_grpA.append(nombre)
-#         elif sexo == "H":
-#             Hombres_grpB.append(nombre)
+        if sexo == "M":
+            Mujeres_grpA.append(nombre)
+        elif sexo == "H":
+            Hombres_grpB.append(nombre)
 
-#         mensaje_continuo = input(
-#             f"""Deseas continuar agregando personas?, (Si) para continuar y (No) para terminar
-#             Te comparto las listas de alumnos segun su sexo ingresado con anterioridad:
-#             Lista de hombres = {Hombres_grpB}
-#             Lista de Mujeres = {Mujeres_grpA} """)
+        mensaje_continuo = input(
+            f"""Deseas continuar agregando personas?, (Si) para continuar y (No) para terminar
+            Te comparto las listas de alumnos segun su sexo ingresado con anterioridad:
+            Lista de hombres = {Hombres_grpB}
+            Lista de Mujeres = {Mujeres_grpA} """)
 
-#         if mensaje_continuo.capitalize() == "Si":
-#             continue
-#         else:
-#             break
+        if mensaje_continuo.capitalize() == "Si":
+            continue
+        else:
+            break
 
-#     seguir = input(
-#         "¿Quieres ingresar más nombres en una nueva lista? (Si/No): ")
-#     if seguir.capitalize() != "Si":
-#         break
+    seguir = input(
+        "¿Quieres ingresar más nombres en una nueva lista? (Si/No): ")
+    if seguir.capitalize() != "Si":
+        break
 # ---------------------------------------------------------------------------------TEST REPETICION EN FOR ------------------------------------------------------------
 
 # nombreUsuario = input("Ingresa tu nombre:")
