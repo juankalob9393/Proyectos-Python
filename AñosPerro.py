eb864be72493a47cb922c3a4b58e7ee5264532be
juankalob9393 <juan08carlos9393@gmail.com>
2023-10-07 13:49:47 -0600

    V1

---
+++ AñosPerro.py
@@ -0,0 +1,26 @@
+x = input("Ingresa el nombre de tu perro: ")
+y = int(input("Ingresa la edad en años de tu perro: "))
+
+if y == 1:
+    yui = "Su edad en humano es de 20 años"
+elif y == 2:
+    yui = "Su edad en humano es de 28 años"
+elif y ==3:
+    yui = "Su edad en humano es de 32 años"
+elif y == 4:
+    yui = "Su edad en humano es de 36 años"
+elif y == 5:
+    yui = "Su edad en humano es de 40 años"
+elif y == 6:
+    yui = "Su edad en humano es de 44 años"
+elif y == 7:
+    yui = "Su edad en humano es de 48 años"
+elif y == 8:
+    yui = "Su edad en humano es de 52 años"
+elif y == 9:
+    yui = "Su edad en humano es de 56 años"
+else:
+    yui = "Supera los 56 años ya esta pagando horas extras"
+  
+print(f"Ya que {x} tiene una edad perruna de {y} años entonces {yui}")
+    
---
+++ Calculadora Basica.py
@@ -0,0 +1,17 @@
+x = input("introduce el primer numero: ")
+operacion = input("Introduce la operacion +,  -, * o /: ")
+y = input("Introduce el segundo numero: ")
+
+x_int = int(x)
+y_int = int(y)
+
+if operacion == "+":
+   resultado = x_int + y_int
+elif operacion == "-":
+    resultado = x_int - y_int
+elif operacion == "/":
+    resultado = x_int / y_int
+else: 
+    resultado = x_int * y_int
+   
+print(f"El resultado de {x_int} {operacion} {y_int} es {resultado}")
