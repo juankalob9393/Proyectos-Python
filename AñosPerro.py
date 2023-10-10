e2ce983c199e36e78ce98f1464a8d976d118af75
juankalob9393 <juan08carlos9393@gmail.com>
2023-10-09 19:28:37 -0600

    V2

--- AñosPerro.py
+++ AñosPerro.py
@@ -1,26 +1,25 @@
-x = input("Ingresa el nombre de tu perro: ")
-y = int(input("Ingresa la edad en años de tu perro: "))
+Nombreperro = input("Ingresa el nombre de tu perro: ").capitalize()
+edadPerro = int(input("Ingresa la edad en años de tu perro: "))
 
-if y == 1:
-    yui = "Su edad en humano es de 20 años"
-elif y == 2:
-    yui = "Su edad en humano es de 28 años"
-elif y ==3:
-    yui = "Su edad en humano es de 32 años"
-elif y == 4:
-    yui = "Su edad en humano es de 36 años"
-elif y == 5:
-    yui = "Su edad en humano es de 40 años"
-elif y == 6:
-    yui = "Su edad en humano es de 44 años"
-elif y == 7:
-    yui = "Su edad en humano es de 48 años"
-elif y == 8:
-    yui = "Su edad en humano es de 52 años"
-elif y == 9:
-    yui = "Su edad en humano es de 56 años"
+if edadPerro == 1:
+    Mensaje = "Su edad en humano es de 20 años"
+elif edadPerro == 2:
+    Mensaje = "Su edad en humano es de 28 años"
+elif edadPerro == 3:
+    Mensaje = "Su edad en humano es de 32 años"
+elif edadPerro == 4:
+    Mensaje = "Su edad en humano es de 36 años"
+elif edadPerro == 5:
+    Mensaje = "Su edad en humano es de 40 años"
+elif edadPerro == 6:
+    Mensaje = "Su edad en humano es de 44 años"
+elif edadPerro == 7:
+    Mensaje = "Su edad en humano es de 48 años"
+elif edadPerro == 8:
+    Mensaje = "Su edad en humano es de 52 años"
+elif edadPerro == 9:
+    Mensaje = "Su edad en humano es de 56 años"
 else:
-    yui = "Supera los 56 años ya esta pagando horas extras"
-  
-print(f"Ya que {x} tiene una edad perruna de {y} años entonces {yui}")
-    
+    Mensaje = "Supera los 56 años ya esta pagando horas extras"
+
+print(f" {Nombreperro} tiene una edad perruna de {edadPerro} años entonces {Mensaje}")
