ea71d8b6d79f39c39bfbf21575719e66bbe8f118
juankalob9393 <juan08carlos9393@gmail.com>
2023-10-09 19:14:31 -0600

    V3

--- calculadora infinita.py
+++ calculadora infinita.py
@@ -1,24 +1,45 @@
 while True:
+    while True:
 
-    operacion = input(
-        "Introduce la operacion (+),(-),(*),(/) tambien introduce (salir) para terminar: ")
-    if operacion.lower() == "salir":
-        break
+        x = (input("introduce el primer numero ó (salir) para terminar: "))
+        x = x.lower()
+        if x == "salir":
+            break
+        else:
+            x = int(x)
 
-    x = int(input("introduce el primer numero: "))
-    y = int(input("Introduce el segundo numero: "))
+        operacion = input(
+            "Introduce la operacion (+),(-),(*),(/) ó (salir) para terminar: ")
+        if operacion.lower() == "salir":
+            break
 
-    if operacion == "+":
-        resultado = x + y
-        print(f"La suma de {x} {operacion} {y} = {resultado}")
-    elif operacion == "-":
-        resultado = x - y
-        print(f"La resta de {x} {operacion} {y} = {resultado}")
-    elif operacion == "/":
-        resultado = x / y
-        print(f"La division de {x} {operacion} {y} = {resultado}")
-    elif operacion == "*":
-        resultado = x * y
-        print(f"La multiplicacion de {x} {operacion} {y} = {resultado}")
-    else:
-        print("Operacion invalida ingresa uno correcto")
+        y = (input("introduce el segundo numero ó (salir) para terminar: "))
+        y = y.lower()
+        if y == "salir":
+            break
+        else:
+            y = int(y)
+
+        if operacion == "+":
+            resultado = x + y
+            print(f"La suma de {x} {operacion} {y} = {resultado}")
+        elif operacion == "-":
+            resultado = x - y
+            print(f"La resta de {x} {operacion} {y} = {resultado}")
+        elif operacion == "/":
+            resultado = x / y
+            print(f"La division de {x} {operacion} {y} = {resultado}")
+        elif operacion == "*":
+            resultado = x * y
+            print(f"La multiplicacion de {x} {operacion} {y} = {resultado}")
+        else:
+            print("Operacion invalida ingresa uno correcto")
+
+        salirCalc = input("Deseas hacer otro calculo rapido? (S/N): ")
+        salircalc = salirCalc.lower()
+        if salirCalc == "s":
+            continue
+        else:
+            break
+
+    break
