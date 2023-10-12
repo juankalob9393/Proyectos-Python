c43fa04f69e27618744a17bee8892c0bfd2047a7
Juan carlos Lopez <juan08carlos9393@gmail.com>
2023-10-12 12:03:42 -0600

    Proyecto en pausa

---
+++ FuncionyWhile.py
@@ -0,0 +1,11 @@
+def Suma(num1, num2):
+    resultado = num1+num2
+    print(resultado)
+
+
+while True:
+    numero1 = int(input("Ingresa un primer numero: "))
+    numero2 = int(input("Ingresa un segundo numero: "))
+
+    Suma(numero1, numero2)
+    break
