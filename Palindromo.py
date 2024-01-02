65dc4b8948f613ecbf667161a0c8238487f14696
Juan Carlos Lopez <juan08carlos9393@gmail.com>
2024-01-02 11:26:05 -0600

    v1

---
+++ Palindromo.py
@@ -0,0 +1,40 @@
+print("FUNCION DE PALINDROMO")
+
+Palindro1 = input("Ingresa tu Oracion o palabra a verificar: ")
+
+
+def espaciosblancos(Frase1):  # ELIMINA ESPACIOS
+    frasenueva = ""
+    for char in Frase1:
+        if char == " ":
+            char = ""
+        else:
+            frasenueva += char
+    return frasenueva
+
+
+print("STOP1")
+l1 = ((espaciosblancos(Palindro1)).lower())
+print(l1, "Elimine los espacios")
+
+
+def voltear(Frase2):  # VOLTEA ORACION O PALABRA
+    return (Frase2[::-1])
+
+
+print("STOP2")
+l2 = voltear(l1)
+print(l2, "Pude Voltear la frase o palabra")
+
+
+def comprobacionFinal(Frase3, Frase4):  # COMPRUEBA SI LA PALABRA ES PALINDROMO O NO
+    if Frase3 == Frase4:
+        a1 = "Si es un palindromo"
+    else:
+        a1 = "No es un palindromo"
+    return a1
+
+
+print("STOP3")
+l3 = comprobacionFinal(l1, l2)
+print(l3)
